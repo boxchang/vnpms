533b28eff34f7c6b8ef5834a03d1136ff4bd61df
Box Chang <forkome@gmail.com>
2024-07-25 15:54:17 +0700

    Update

--- src/VNPMS/problems/ser_views.py
+++ src/VNPMS/problems/ser_views.py
@@ -1,6 +1,7 @@
 from rest_framework.mixins import ListModelMixin
 from rest_framework.viewsets import GenericViewSet
 
+from bases.models import Status
 from bugs.models import Bug
 from problems.models import Problem
 from problems.serializers import ProblemSerializer
@@ -12,7 +13,7 @@ class ProblemByPViewSet(ListModelMixin, GenericViewSet):
 
     def get_queryset(self):
         p_pk = self.kwargs['project_id']
-        exclude_list = [6, 8]
+        exclude_list = Status.objects.filter(status_en__in=['Done', 'On-Going'])
         queryset = Problem.objects.filter(project=p_pk).exclude(problem_status__in=exclude_list)
 
         return queryset
