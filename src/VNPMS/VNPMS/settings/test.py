98a420279e912bc04b5e84c3ea3fe32c77938ad1
Box Chang <forkome@gmail.com>
2024-12-18 09:52:30 +0700

    Update

--- src/VNPMS/VNPMS/settings/test.py
+++ src/VNPMS/VNPMS/settings/test.py
@@ -2,7 +2,7 @@
 
 from .base import *
 
-SECRET_KEY = '+e9tzio&ivf94+ek0$_9l8op)gxc4r+t9pen@dov0j7c4zks%r'
+SECRET_KEY = '-e9tzio&ivf94+ek0$_9l8op)gxc4r+t9pen@dov0j7c4zks%r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
